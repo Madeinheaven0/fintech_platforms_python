efa0a159ff1158837b89a96f5fb3fdb1fb5065ae
EPEE EPEE Landry <landryepee5@gmail.com>
2026-09-05 23:42:34 +0100

    Parser and analitycs class is finished

---
+++ data/market_data.py
@@ -0,0 +1,99 @@
+import csv
+import json
+from pathlib import Path
+from typing import List, Dict, Union
+
+
+
+class MarketDataParser:
+    def __init__(self) -> None:
+        self.data: Dict[str, List[Dict[str, Union[str, float]]]] = {}
+
+    def _clean_row(self, row: dict) -> dict:
+        """
+        Convert a string into number if it's possible
+        :param row: the dictionary to convert
+        :return: the cleaned dictionary
+        """
+        cleaned_row = {}
+        for key, value in row.items():
+            try:
+                cleaned_row[key] = float(value) is '.' in str(value) or int(value)
+            except (ValueError, TypeError):
+                cleaned_row[key] = value
+
+        return cleaned_row
+
+    def parse_csv_file(self, pathfile: Path, default_ticker = None):
+        """
+        Parse a CSV file and convert it into a list of dictionaries (multi-asset or mono-assets)
+        :param pathfile:
+        :param default_ticker:
+        :return:
+        """
+        with open(file=pathfile, mode="r", encoding="utf-8") as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                row_cleaned = self._clean_row(row)
+
+                ticker = row_cleaned.pop("Ticker", None) or row_cleaned.pop("symbol", None) or default_ticker
+
+                if not ticker:
+                    raise ValueError("Cannot find ticker in the CSV file")
+
+                ticker = str(ticker).upper()
+                if ticker not in self.data:
+                    self.data[ticker] = []
+                    self.data[ticker].append(row_cleaned)
+
+    def parse_json_file(self, pathfile: Path, default_ticker = None):
+        """Parse a JSON file and convert it into a list of dictionaries (multi-objects or mono-object)"""
+        with open(file=pathfile, mode="r", encoding="utf-8") as json_file:
+            content = json.load(json_file)
+
+            records = content if isinstance(content, list) else [content]
+
+            for row in records:
+                row_cleaned = self._clean_row(row)
+
+                ticker = row_cleaned.pop("Ticker", None) or row_cleaned.pop("symbol", None) or default_ticker
+
+                if not ticker:
+                    raise ValueError("Cannot find ticker in the JSON file")
+
+                ticker = str(ticker).upper()
+
+                if ticker not in self.data:
+                    self.data[ticker] = []
+                self.data[ticker].append(row_cleaned)
+
+    def load_path(self, path_str: str):
+        """Load the files and convert them"""
+        path = Path(path_str)
+
+        if path.is_file():
+            files = [path]
+        elif path.is_dir():
+            files = list(path.glob("*.csv")) + list(path.glob("*.json"))
+        else:
+            raise FileNotFoundError(f"Not found the path : {path_str}")
+
+        for file in files:
+            # Si the name of the file is the style 'AAPL.csv', we extract 'AAPL' as the default ticker
+            default_ticker = file.stem.upper()
+
+            if file.suffix.lower() == ".csv":
+                self.parse_csv_file(file, default_ticker=default_ticker)
+            elif file.suffix.lower() == ".json":
+                self.parse_json_file(file, default_ticker=default_ticker)
+
+
+if __name__ == "__main__":
+    parser = MarketDataParser()
+
+    # load an unique multi-assets file or a complete folder
+    # parser.load_path("donnees_marche.csv")
+    # parser.load_path("./dossier_fichiers_actifs/")
+
+    # aapl_prices = parser.data.get("AAPL", [])
+    # print(f"Number of candle for AAPL : {len(aapl_prices)}")
---
+++ src/analytics.py
@@ -0,0 +1,138 @@
+import polars as pl
+from typing import Dict, Union, List, Optional
+from pathlib import Path
+import os
+
+from data.market_data import MarketDataParser
+
+
+class AnalyticsData:
+    """
+        Analytics class who compute basics informations and inform the user
+        if threshold are passed
+    """
+    def __init__(self, pathfile: str) -> None:
+        """
+        Analytics data class constructor
+        :param pathfile: the path of the raw data file (json or csv)
+        """
+        self.market_data = MarketDataParser()
+        self.pathfile = pathfile
+        self._asset_data = {}
+        self._dataset: Optional[pl.DataFrame] = None
+        self._metrics: Dict[str, Dict[str, float | List[float]]] = {}
+        self._alerts: Dict[str, Dict[str, str]] = {}
+        self._window: int = 0
+
+    def load_data(self) -> None:
+        self._asset_data = self.market_data.load_path(self.pathfile)
+        self._dataset = pl.DataFrame(self._asset_data)
+
+    def _compute_returns(self, df: pl.DataFrame) -> pl.DataFrame:
+        """Comput the daily log returs"""
+        return df.with_columns(
+            (pl.col("close") / pl.col("close").shift(1))
+            .log()
+            .fill_null(0.0)
+            .alias("log_return")
+        )
+
+    def _compute_rolling_means(self, window: int, df: pl.DataFrame) -> pl.DataFrame:
+        """Compute the rolling meean"""
+        return df.with_columns(
+            pl.col("close")
+            .rolling_mean(window)
+            .alias(f"rolling_mean_{window}")
+        )
+
+    def _compute_annalized_volatility(self, window: int, trading_days: int, df: pl.DataFrame) -> pl.DataFrame:
+        """Compute the annalized volatility"""
+        if 'log_returns' in self._dataset.columns:
+            self._compute_returns(df=df)
+        return df.with_columns(
+            (
+            pl.col("log_returns")
+             .rolling(window) * (trading_days ** 0.5))
+            .alias(f"annualized_volatility_{window}")
+        )
+
+    def compute_metrics(self, window: int = 20, trading_days: int = 252) -> None:
+        self._window = window
+
+        if not self._asset_data:
+            self.load_data()
+
+        processed_df = []
+        for asset, data in self._asset_data.items():
+            if not data:
+                continue
+
+            df = pl.DataFrame(data)
+
+            if "ticker" not in df.columns:
+                df = df.with_columns(pl.lit(asset).alias("ticker"))
+
+            if "date" in df.columns:
+                df = df.sort("date")
+
+            df = self._compute_returns(df=df)
+            df = self._compute_rolling_means(window=window, df=df)
+            df = self._compute_annalized_volatility(window=window, df=df, trading_days=trading_days)
+            processed_df.append(df)
+
+            vol_col = f"annualized_volatility_{window}"
+            last_vol = df[vol_col].drop_nulls().tail(1)
+
+            self._metrics[asset] = {
+                "returns": df["log_return"].to_list(),
+                "total_returns": float(df["log_return"].sum()),
+                "rolling_means": df[f"rolling_mean_{window}"].to_list(),
+                "annualized_volatility": df[vol_col].to_list(),
+                "last_volatility": float(last_vol[0]) if len(last_vol) > 0 else 0.0,
+            }
+        if processed_df:
+            self._dataset = pl.concat(processed_df)
+
+    def compute_alerts(self, vol_threshold: float, returns_threshold: float) -> None:
+        if not self._metrics:
+            self.compute_metrics()
+
+        for asset in self._asset_data.keys():
+            self._alerts[asset] = {}
+            if self._metrics[asset]["total_returns"] < returns_threshold:
+                self._alerts[asset]["total_returns"] = "The total returns is too low"
+            if self._metrics[asset][f"annualized_volatility_{self._window}"] < vol_threshold:
+                self._alerts[asset][f"annualized_volatility_{self._window}"] = "The annualized volatility is too high"
+
+    def save_dataset_to_parquet(self, pathfile: Path, name: str):
+        if pathfile.exists():
+            path = pathfile / f"{name}.parquet"
+            self._dataset.write_parquet(path)
+        else:
+            os.makedirs(pathfile)
+            path = pathfile / f"{name}.parquet"
+            self._dataset.write_parquet(path)
+
+    @property
+    def metrics(self) ->  Dict[str, Dict[str, float | List[float]]]:
+        return self._metrics
+
+    @property
+    def alerts(self) -> Dict[str, Dict[str, str]]:
+        return self._alerts
+
+    @property
+    def asset_data(self) -> Dict[str, Dict[str, Dict[str, Union[str, float]]]] | str:
+        if not self.asset_data:
+            self.load_data()
+        return self._asset_data
+
+    @property
+    def dataset(self) -> pl.DataFrame | None:
+        if self._dataset is None:
+            self.compute_metrics()
+        return self._dataset
+
+
+
+
